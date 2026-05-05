dabaf9495c538c90c6ea377e4fa56126dc18ca06
sacc2026 <moopy@a-star.edu.sg>
2026-05-05 16:45:48 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,50 +7,45 @@ export default function RootLayout({ children }) {
         <header className="topbar">
           <div className="topbar-container">
 
-            <a href="/">
+            {/* Logo */}
+            <a href="/" className="logo-link">
               <img
                 src="/header-logo.png"
                 alt="SAAC 2026 Logo"
                 className="header-logo"
               />
-            </a
+            </a>
 
             {/* Navigation */}
             <nav className="nav">
 
-              {/* ABOUT dropdown */}
               <div className="dropdown">
-                #
+                <a href="#">
                   ABOUT SAAC <span className="arrow">▼</span>
                 </a>
                 <div className="dropdown-menu">
-                  About the Conference
-                  Organising Committee
+                  <a href="#">About the Conference</a>
+                  <a href="#">Organising Committee</a>
                 </div>
               </div>
 
-              {/* Speakers */}
-              Speakers
-              </a>
+              <a href="/speakers">Speakers</a>
 
-              {/* Programme dropdown */}
               <div className="dropdown">
-                #
+                <a href="#">
                   PROGRAMME DETAILS <span className="arrow">▼</span>
                 </a>
                 <div className="dropdown-menu">
-                  Technical Programme
-                  Schedule
+                  <a href="/program">Technical Programme</a>
+                  <a href="/program">Schedule</a>
                 </div>
               </div>
 
-              {/* Registration */}
-              Registration
+              <a href="https://www.eventbrite.com" target="_blank">
+                Registration
               </a>
 
-              {/* Call for Abstracts */}
-              Call for Abstracts
-              </a>
+              <a href="#">Call for Abstracts</a>
 
             </nav>
           </div>
