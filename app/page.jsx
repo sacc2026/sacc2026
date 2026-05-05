d865de9609bb3aa89c153bcd0d1220d34eed7269
sacc2026 <moopy@a-star.edu.sg>
2026-05-05 15:19:36 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,20 @@
 export default function HomePage() {
   return (
     <>
-      <section className="hero">
-        <h2>Southeast Asia Catalysis Conference 2026</h2>
-        <p>10–11 December 2026 · NUS U‑Town · Singapore</p>
+     <section className="hero">
+  <h2>Southeast Asia Catalysis Conference 2026</h2>
+  <p>10–11 December 2026 · NUS U‑Town · Singapore</p>
 
-        https://www.eventbrite.com/
-          Register Now
-        </a>
-      </section>
-
+  <a
+    href="https://www.eventbrite.com/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="button"
+  >
+    Register Now
+  </a>
+</section>
+``
       <section>
         <h3>About the Conference</h3>
         <p>
