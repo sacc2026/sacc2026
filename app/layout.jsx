b142beebe0db482deaf7b1df23fe6fdd0329d800
sacc2026 <moopy@a-star.edu.sg>
2026-05-06 08:48:50 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,10 +4,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header style={{ padding: "20px", background: "#0b1f4a", color: "white" }}>
-          /
-            SAAC 2026
-          </a>
+        <header style={{ padding: "20px", background: "#0b1f4a", color: "#fff" }}>
+          <a href="/">SAAC 2026</a>
         </header>
 
         <main>{children}</main>
